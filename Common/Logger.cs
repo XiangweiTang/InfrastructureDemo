8956cdb753f30cfbb4838ddf4bfa401621eafbcd
tangxiangwei@gmail.com <tangxiangwei@gmail.com>
2020-06-13 19:49:13 +0800

    Update

---
+++ Common/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common
+{
+    public static class Logger
+    {
+        public static string LogPath { get; set; } = "Log.txt";
+        public static string ErrorPath { get; set; } = "Error.txt";
+        public static void WriteLine(string content, bool inError=false,bool inLog = true)
+        {
+            DateTime dt = DateTime.Now;
+            string s = $"{dt.ToStringLog()}\t{content}";
+            Console.WriteLine(s);
+            if (inLog)
+                File.AppendAllLines(LogPath, s.ToSequence());
+            if (inError)
+                File.AppendAllLines(ErrorPath, s.ToSequence());
+        }
+    }
+}
---
+++ Common/StringOp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class StringOp
+    {
+        public static string ToStringLog(this DateTime dt)
+        {
+            return dt.ToString("yyyyMMdd hh:mm:ss");
+        }
+    }
+}
