5428d9582de706522757c1f2ba712f81ef3a0283
tangxiangwei@gmail.com <tangxiangwei@gmail.com>
2020-06-21 15:53:32 +0800

    Add 2D linear regression.

---
+++ Common/LinearRegression.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class LinearRegression
+    {
+        public double SumX { get; private set; } = 0;
+        public double SumY { get; private set; } = 0;
+        public double SumXY { get; private set; } = 0;
+        public double SumX2 { get; private set; } = 0;
+        public double MeanX { get; private set; } = 0;
+        public double W { get; private set; } = 0;
+        public double B { get; private set; } = 0;
+        public double M { get; private set; } = 0;
+        private IEnumerable<PlainVector> Vectors = null;
+        public LinearRegression(IEnumerable<PlainVector> vectors)
+        {
+            Vectors = vectors;
+        }
+        public void Calc()
+        {
+            SumX = 0;
+            SumY = 0;
+            SumXY = 0;
+            SumX2 = 0;
+            foreach(var vector in Vectors)
+            {
+                SumX += vector.X;
+                SumY += vector.Y;
+                SumXY += vector.X * vector.Y;
+                SumX2 += vector.X * vector.X;
+                M++;
+            }
+
+            MeanX = SumX / M;
+            W = (SumXY - MeanX * SumY) / (SumX2 - SumX * SumX / M);
+            B = (SumY - W * SumX) / M;
+        }
+    }
+}
