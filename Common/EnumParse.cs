07454bf6ad6d7cca09087f8adebb32dbd056dae7
tangxiangwei@gmail.com <tangxiangwei@gmail.com>
2020-10-02 13:20:54 +0800

    Init some of the common classes. Add the Line class. Add the LogLine Class. Add a specific class for parse enum. Add the Sequence class. Update the logger class.

---
+++ Common/EnumParse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class EnumParse
+    {
+        public static LogLineType ParseLogLineString(string s)
+        {
+            switch (s.ToLower())
+            {
+                case "na":
+                    return LogLineType.NA;
+                case "log":
+                    return LogLineType.Log;
+                case "internalerror":
+                    return LogLineType.InternalError;
+                case "status":
+                    return LogLineType.Status;
+                default:
+                    throw new InfException($"Invalid LogLineType: {s}");
+            }
+        }
+    }
+}
---
+++ Common/Line/LogLine.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// The Line structure for log.
+    /// </summary>
+    public class LogLine:Line
+    {
+        /// <summary>
+        /// The type of the log.
+        /// </summary>
+        public LogLineType LogType { get; set; } = LogLineType.NA;
+        /// <summary>
+        /// The timestamp of the log.
+        /// </summary>
+        public DateTime TimeStamp { get; set; }
+        /// <summary>
+        /// The content of the log.
+        /// </summary>
+        public string Content { get; set; } = "";
+        /// <summary>
+        /// Create a new instance of the LogLine.
+        /// </summary>
+        /// <param name="t">The type of the log.</param>
+        /// <param name="content">The content of the log.</param>
+        public LogLine(LogLineType t, string content):base()
+        {
+            LogType = t;
+            TimeStamp = new DateTime();
+            Content = content;
+        }
+        /// <summary>
+        /// Create a new instance of the LogLine.
+        /// </summary>
+        /// <param name="line">The full log line string.</param>
+        public LogLine(string line) : base(line) { }
+        /// <summary>
+        /// Implement the abstract function GetLine().
+        /// </summary>
+        /// <returns>The sequence of LogLine parts.</returns>
+        protected override IEnumerable<object> GetLine()
+        {
+            yield return LogType;
+            yield return TimeStamp;
+            yield return Content;
+        }
+        /// <summary>
+        /// Implement the abstract function SetLine().
+        /// </summary>
+        /// <param name="split">The split parts of the input line string.</param>
+        protected override void SetLine(string[] split)
+        {
+            LogType = EnumParse.ParseLogLineString(split[0]);
+            TimeStamp = DateTime.Parse(split[1]);
+            Content = split[2];
+        }
+    }    
+    /// <summary>
+    /// The differnet types of LogLine.
+    /// </summary>
+    public enum LogLineType
+    {
+        /// <summary>
+        /// Default type.
+        /// </summary>
+        NA=0,
+        /// <summary>
+        /// Plain log.
+        /// </summary>
+        Log=1,
+        /// <summary>
+        /// Internal error.
+        /// </summary>
+        InternalError=2,
+        /// <summary>
+        /// Status of the current memory/CPU...
+        /// </summary>
+        Status=3,
+    }
+}
