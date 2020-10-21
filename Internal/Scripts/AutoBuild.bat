4c5abc6f823ee295ba03e76edf98b49b8feec16b
tangxiangwei@gmail.com <tangxiangwei@gmail.com>
2020-10-22 00:56:14 +0800

    fix

--- Internal/Scripts/AutoBuild.bat
+++ Internal/Scripts/AutoBuild.bat
@@ -2,6 +2,6 @@ set reposPath=%USERPROFILE%\source\repos\InfrastructureDemo
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 pushd %reposPath%
 git checkout -- .
-git checkout backend
+git checkout master
 git pull
 msbuild  %reposPath%\InfrastructureDemo.sln /t:Clean,Build 
